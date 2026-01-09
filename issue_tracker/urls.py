6f9f6622e5fbcfd89e5142787443a7bc56d9d813
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2026-01-09 16:01:36 +0530

    feat: initialize Django project with settings

---
+++ issue_tracker/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for issue_tracker project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+
+    # API endpoints
+    path('api/', include('tracker.urls')),
+
+    # API Documentation (Swagger/OpenAPI)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Root redirect to docs
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='home'),
+]
